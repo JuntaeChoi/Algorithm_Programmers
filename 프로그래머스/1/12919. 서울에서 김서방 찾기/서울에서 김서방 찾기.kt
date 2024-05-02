72af8b489f60f8756eafd44218ee8b1ab29c5979
Juntae Choi <31373739+chlwnsxo00@users.noreply.github.com>
2024-05-03 01:37:05 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 24.08 ms, Memory: 63.7 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.kt
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.kt
@@ -1,11 +1,5 @@
 class Solution {
     fun solution(seoul: Array<String>): String {
-        var count = 0
-        for (i in seoul){
-            if (i.equals("Kim"))
-                return "김서방은 ${count}에 있다"
-            count++
-        }
-        return ""
+        return "김서방은 ${seoul.indexOf("Kim")}에 있다"
     }
 }
