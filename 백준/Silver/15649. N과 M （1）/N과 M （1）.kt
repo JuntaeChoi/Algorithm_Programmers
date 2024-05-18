076a1be8dafa942a3db98dfabb141bca714ce073
Juntae Choi <31373739+chlwnsxo00@users.noreply.github.com>
2024-05-18 23:23:54 +0900

    [Silver III] Title: N과 M (1), Time: 2508 ms, Memory: 85672 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.kt
@@ -0,0 +1,30 @@
+import java.util.*
+lateinit var visited : BooleanArray
+lateinit var arr : IntArray
+fun main(args: Array<String>) = with(System.`in`.bufferedReader()) {
+    val nums = readLine().split(" ")
+    val n = nums[0].toInt()
+    val m = nums[1].toInt()
+    visited = BooleanArray(n){false}
+    arr = IntArray(m)
+    DFS(n,m,0)
+}
+
+fun DFS(n:Int,m:Int,depth:Int){
+    if(depth==m){
+        arr.forEach {
+            print("$it ")
+        }
+        println()
+    }
+    else if (depth<m){
+        for(i in 0 until n){
+            if(!visited[i]){
+                arr[depth]=i+1
+                visited[i]=true
+                DFS(n,m,depth+1)
+                visited[i]=false
+            }
+        }
+    }
+}
